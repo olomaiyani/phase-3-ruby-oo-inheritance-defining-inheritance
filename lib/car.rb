ebb6ae3fd41383f5983002526dec4fe903d89449
Nana <nanaolomaiyani@gmail.com>
2022-10-25 21:17:06 +0300

    Completed assignment

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,14 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+    def initialize(wheel_size,wheel_number)
+        @wheel_number = wheel_number
+        @wheel_size = wheel_size
+    end
+    def go
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end
+    def fill_up_tank
+        "filling up!"
+    end
+end
